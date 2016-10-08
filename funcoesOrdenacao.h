78d8041c3700119ce115e0b7fcea80af954677df
tonyfryar <tonyfryar@gmail.com>
2016-10-08 12:50:01 -0300

    Update funcoesOrdenacao.h

--- funcoesOrdenacao.h
+++ funcoesOrdenacao.h
@@ -1,15 +1,24 @@
+
+
+
 #ifndef FUNCOESORDENACAO_H
 #define FUNCOESORDENACAO_H
 
 #define MAXPALAVRAS 1000
 
-void insertionSort(int* vetor,int fim ,int inicio);
+using namespace std;
+
+void troca (string vetor[],int esq, int dir);
 //void troca (int vetor[], int esq, int dir);
+void ordenarTextoSelection(char* argv);
+void ordenarTextoInsertion(char* argv);
+void ordenarTextoQuick(char* argv);
+
 void quickSort(int *vet, int esq, int dir);
 void selection_sort(int num[], int tam);
+void insertionSort(int* vetor,int fim ,int inicio);
 
-/*void insertionSort(string vetor[], int fim, int inicio);
-void quickSort(string vetor[], int fim, int inicio);
-void troca (string vetor[], int esq, int dir);
-*/
+void insertionSortString(string vetor[], int fim, int inicio);
+void quickSortString(string vetor[],int fim, int inicio);
+
 #endif 
